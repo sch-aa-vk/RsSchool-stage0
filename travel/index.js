ad8dbb1a9ba54a1664a42121069d16c9bc5c6ef0
Senchenko Amina <sch.aa.vk@gmail.com>
2022-07-28 13:43:57 +0600

    fix: fix hamburger menu, header and preview part

--- travel/index.js
+++ travel/index.js
@@ -1,1 +1,26 @@
 console.log("1. Верстка валидная 10/10\n2. Верстка семантическая 20/20\n3. Верстка соответствует макету 48/48\n4. Требования к css соблюдены 12/12\n5. Есть интерактивность 20/20");
+
+const hamburger = document.querySelector('.hamburger');
+const line = document.querySelector('.line');
+const menu = document.querySelector('.hamburger_list');
+const body = document.querySelector('body');
+
+function openMenu() {
+    hamburger.classList.toggle('open');
+    menu.classList.toggle('open');
+    line.classList.toggle('open');
+    body.classList.toggle('fixed-page');
+}
+
+hamburger.addEventListener('click', openMenu);
+
+const navLinks = document.querySelectorAll('.header__link');
+
+function closeMenu() {
+    hamburger.classList.remove('open');
+    menu.classList.remove('open');
+    line.classList.remove('open');
+    body.classList.remove('fixed-page');
+}
+
+navLinks.forEach((el) => el.addEventListener('click', closeMenu));
